4b5f14f97c2953cd27dc5802ffcc1bda839319ec
Masekela Lerato <leratomasekela234@gmail.com>
2023-12-10 23:37:41 +0200

    completed the app

--- To Do List/src/Login/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://quwuhspddvijumtmnmuf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1d3Voc3BkZHZpanVtdG1ubXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyMzIyMDgsImV4cCI6MjAxNzgwODIwOH0.ML8DdbcPDMvcw8P6sGVsNtJzqFg_WYkPxSh1eyHtJU0';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
